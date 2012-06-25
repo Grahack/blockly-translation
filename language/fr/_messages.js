22099527407d004c971e966ba479f922f8f44cd4
Gildas Cuisinier <gildas.cuisinier@gmail.com>
2012-06-25 21:51:28 +0200

    add empty readme

---
+++ language/fr/_messages.js
@@ -0,0 +1,81 @@
+// Text strings (factored out to make multi-language easier).
+
+// Context menus.
+Blockly.MSG_REMOVE_COMMENT = 'Remove Comment';
+Blockly.MSG_ADD_COMMENT = 'Add Comment';
+Blockly.MSG_EXTERNAL_INPUTS = 'External Inputs';
+Blockly.MSG_INLINE_INPUTS = 'Inline Inputs';
+Blockly.MSG_DELETE_BLOCK = 'Delete Block';
+Blockly.MSG_DELETE_X_BLOCKS = 'Delete %1 Blocks';
+Blockly.MSG_COLLAPSE_BLOCK = 'Collapse Block';
+Blockly.MSG_EXPAND_BLOCK = 'Expand Block';
+Blockly.MSG_HELP = 'Help';
+
+// Variable renaming.
+Blockly.MSG_CHANGE_VALUE_TITLE = 'Change value:';
+Blockly.MSG_NEW_VARIABLE = 'New variable...';
+Blockly.MSG_NEW_VARIABLE_TITLE = 'New variable name:';
+Blockly.MSG_RENAME_VARIABLE = 'Rename variable...';
+Blockly.MSG_RENAME_VARIABLE_TITLE = 'Rename all "%1" variables to:';
+
+// Toolbox.
+Blockly.MSG_VARIABLE_CATEGORY = 'Variables';
+Blockly.MSG_PROCEDURE_CATEGORY = 'Procedures';
+
+// Mutator dialog.
+Blockly.MSG_MUTATOR_TOOLTIP = 'Edit this block';
+Blockly.MSG_MUTATOR_HEADER = 'Block Editor';
+Blockly.MSG_MUTATOR_CHANGE = 'Change';
+Blockly.MSG_MUTATOR_CANCEL = 'Cancel';
+
+// Variables Blocks
+Blockly.LANG_VARIABLES_GET_HELPURL = 'http://en.wikipedia.org/wiki/Variable_(computer_science)';
+Blockly.LANG_VARIABLES_GET_TITLE_1 = 'get';
+Blockly.LANG_VARIABLES_GET_ITEM = 'item';
+Blockly.LANG_VARIABLES_GET_TOOLTIP_1 = 'Returns the value of this variable.';
+
+Blockly.LANG_VARIABLES_SET_HELPURL = 'http://en.wikipedia.org/wiki/Variable_(computer_science)';
+Blockly.LANG_VARIABLES_SET_TITLE_1 = 'set';
+Blockly.LANG_VARIABLES_SET_ITEM = 'item';
+Blockly.LANG_VARIABLES_SET_TOOLTIP_1 = 'Sets this variable to be equal to the input.';
+
+// Procedures Blocks
+Blockly.LANG_PROCEDURES_DEFNORETURN_HELPURL = 'http://en.wikipedia.org/wiki/Procedure_%28computer_science%29';
+Blockly.LANG_PROCEDURES_DEFNORETURN_PROCEDURE = 'procedure';
+Blockly.LANG_PROCEDURES_DEFNORETURN_DO = 'do';
+Blockly.LANG_PROCEDURES_DEFNORETURN_TOOLTIP_1 = 'A procedure with no return value.';
+
+Blockly.LANG_PROCEDURES_DEFRETURN_HELPURL = 'http://en.wikipedia.org/wiki/Procedure_%28computer_science%29';
+Blockly.LANG_PROCEDURES_DEFRETURN_PROCEDURE = 'procedure';
+Blockly.LANG_PROCEDURES_DEFRETURN_DO = 'do';
+Blockly.LANG_PROCEDURES_DEFRETURN_RETURN = 'return';
+Blockly.LANG_PROCEDURES_DEFRETURN_TOOLTIP_1 = 'A procedure with a return value.';
+
+Blockly.LANG_PROCEDURES_CALLNORETURN_HELPURL = 'http://en.wikipedia.org/wiki/Procedure_%28computer_science%29';
+Blockly.LANG_PROCEDURES_CALLNORETURN_CALL = 'call';
+Blockly.LANG_PROCEDURES_CALLNORETURN_PROCEDURE = 'procedure';
+Blockly.LANG_PROCEDURES_CALLNORETURN_TOOLTIP_1 = 'Call a procedure with no return value.';
+
+Blockly.LANG_PROCEDURES_CALLRETURN_HELPURL = 'http://en.wikipedia.org/wiki/Procedure_%28computer_science%29';
+Blockly.LANG_PROCEDURES_CALLRETURN_CALL = 'call';
+Blockly.LANG_PROCEDURES_CALLRETURN_PROCEDURE = 'procedure';
+Blockly.LANG_PROCEDURES_CALLRETURN_TOOLTIP_1 = 'Call a procedure with a return value.';
+
+// Control
+Blockly.LANG_CONTROL_CATEGORY='Control';
+
+
+// IF CONTROL
+Blockly.LANG_CONTROL_IF_IF_LABEL='if';
+Blockly.LANG_CONTROL_IF_ELSEIF_LABEL='else if';
+Blockly.LANG_CONTROL_IF_ELSE_LABEL='else';
+Blockly.LANG_CONTROL_IF_THEN_LABEL='then';
+
+
+// WHILE/UNTIL CONTROL
+Blockly.LANG_CONTROL_REPEAT_LABEL='repeat';
+Blockly.LANG_CONTROL_WHILE_LABEL='while';
+Blockly.LANG_CONTROL_UNTIL_LABEL='until';
+Blockly.LANG_CONTROL_DO_LABEL='do';
+
+
